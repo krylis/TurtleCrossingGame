65cd83a518556936af48f623cbff01745bd8c0ea
krylis <cmcshann@gmail.com>
2023-09-08 21:36:37 -0700

    Added player, car manager, and scoreboard classes. Added player to bottom of screen and added functionality to move the player up.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from turtle import Screen
+from car_manager import CarManager
+from player import Player
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -6,10 +9,11 @@ screen.setup(width=600, height=600)
 screen.title("Turtle Crossing")
 screen.tracer(0)
 
-game_is_on = True
+player = Player()
+screen.onkey(player.move, "Up")
+screen.listen()
 
+game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
-screen.exitonclick()
